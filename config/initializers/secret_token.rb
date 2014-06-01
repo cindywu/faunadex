b29936bc48964e8335c5fa76f4a2c53a2a97f72d
Cindy Wu <ciindy.wu@gmail.com>
2014-05-31 17:46:27 -0700

    while being trained a pokemon usually stays inside it's pokeball

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faunadex::Application.config.secret_key_base = 'de03d12460c2a6a16bb3658def74f76f59a8d3526d2cd0d28189596f6aa67a9fd9a4799625936416dcd9cc83e56f9bd50637f283ee95d3214215fa8453341a0c'
